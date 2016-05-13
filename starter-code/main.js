77f5465f0802cf51d21cbf8e353c857aa3b33d03
Samuel Weisse <Samuel Weisse>
2016-05-12 20:03:52 -0700

    updated files

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,28 @@
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo= "queen";
 var cardThree= "king";
-var cardFour= "king";
+var cardFour= "king"; */
+
+
+
+function createBoard(){
+var gameBoard = document.getElementById("game-board");
+
+for (var i = 0; i<4; i++) {
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	gameBoard.appendChild(newCard);
+}
+
+
+}
+
+
+createBoard();
+
+
+/*if (cardTwo === cardFour) {
+    alert('Sorry, try again.');
+} else { 
+    alert('You found a match!');
+}*/
